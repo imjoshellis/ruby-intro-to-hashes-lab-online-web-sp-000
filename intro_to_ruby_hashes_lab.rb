2c50436635d84111aa342413ca28bb7a7def3a06
Josh Ellis <joshe523@gmail.com>
2020-03-03 05:55:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,11 +25,11 @@ def monopoly_with_third_tier
 		railroads: {
 			pieces: 4, names: {
 				reading_railroad: {}
-				}, 
+				},
 				rent_in_dollars: {
-					one_piece_owned: 25, 
-					two_pieces_owned: 50, 
-					three_pieces_owned: 100, 
+					one_piece_owned: 25,
+					two_pieces_owned: 50,
+					three_pieces_owned: 100,
 					four_pieces_owned: 200
 				}
 		}
@@ -43,9 +43,9 @@ def monopoly_with_fourth_tier
 				reading_railroad: {}
 				}, 
 				rent_in_dollars: {
-					one_piece_owned: 25, 
-					two_pieces_owned: 50, 
-					three_pieces_owned: 100, 
+					one_piece_owned: 25,
+					two_pieces_owned: 50,
+					three_pieces_owned: 100,
 					four_pieces_owned: 200
 				}
 		}
